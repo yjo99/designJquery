36cb33f83a213c1b50dd457f9c3ba6c25bdc40e1
YousefSayed <yousef16992@gmail.com>
2023-01-24 21:42:53 +0200

    add save Object

--- object.js
+++ object.js
@@ -20,9 +20,40 @@ class User {
     getPhone() { return this.#phone }
 }
 
-var users = [new User("Ahmed Ali", "01245879632", "ahmed@gamil.com", "Male"),
-            new User("Asmaa Mohamed", "0114786523", "asmaa@gmail.com", "Female"),
-            new User("Khaled Abbas", "01025874563", "khaled@gmail.com", "Male"),
-            new User("Merna Mohamed", "01598745632", "merna@gmail.com", "Female")];
+
+
+
+var users = [];
+
+/// id element 
+var saveBtn = document.getElementById("split")
+
+//form element
+var name = document.getElementById("nameInput")
+var number = document.getElementById("numberInput")
+var email = document.getElementById("emailInput")
+var gender = document.getElementById("slider-flip-m")
+
+
+
+
+
+saveBtn.onclick =  function(){
+
+    var obj = new User(name.value, number.value,email.value, gender.value )
+    users.push(obj);
+    console.log("done")
+    
+}
+console.log(users)
+
+ 
+
+
+
+// var users = [new User("Ahmed Ali", "01245879632", "ahmed@gamil.com", "Male"),
+//             new User("Asmaa Mohamed", "0114786523", "asmaa@gmail.com", "Female"),
+//             new User("Khaled Abbas", "01025874563", "khaled@gmail.com", "Male"),
+//             new User("Merna Mohamed", "01598745632", "merna@gmail.com", "Female")];
 
 
