7a02a1a7777b3c007290ca55a55eb7581fa6bec9
Mohamed Yasser <mohamedkaoud865@gmail.com>
2023-01-26 17:09:10 +0200

    edit & how update

--- User.js
+++ User.js
@@ -36,24 +36,21 @@ class User {
   }
 }
 
+var currentIndex = -1;
 var users = [
-  new User("Ahmed Ali", "01245879632", "ahmed@gamil.com", "Male"),
-  new User("Khaled Abbas", "01025874563", "khaled@gmail.com", "Male"),
-  new User("Asmaa Mohamed", "0114786523", "asmaa@gmail.com", "Female"),
-  new User("Merna Mohamed", "01598745632", "merna@gmail.com", "Female"),
+  new User("Ahmed Ali", "01245879632", "ahmed@gamil.com", "MALE"),
+  new User("Khaled Abbas", "01025874563", "khaled@gmail.com", "MALE"),
+  new User("Asmaa Mohamed", "0114786523", "asmaa@gmail.com", "FEMALE"),
+  new User("Merna Mohamed", "01598745632", "merna@gmail.com", "FEMALE"),
 ];
+
+// retun the list of users
 function getList() {
   users = users.sort((a, b) => a.getName() > b.getName() ? 1 : -1);
   var result = "";
   for (let i = 0; i < users.length; i++) {
-
     result += ' <li><a href="#contactInfo" onclick=showUser(' + i + ')>';
-
-    if (users[i].getGender().toUpperCase() === "MALE")
-      result += '<img src="assets/img/male.png">';
-    else
-      result += '<img src="assets/img/female.png">';
-
+    result += `<img src=${getimgFor(users[i].getGender().toUpperCase())}>`;
     result += `<h2>${users[i].getName()}</h2></a>`;
     result += `<a href="tel:${users[i].getPhone}" data-rel="popup" data-position-to="window" data-transition="pop" data-icon="phone"></a>`;
     result += "</li> ";
@@ -61,37 +58,56 @@ function getList() {
   return result;
 }
 
+// show user info
 function showUser(index) {
+  currentIndex  = index;
   $('#nameDel').val(users[index].getName());
   $('#phoneDel').val(users[index].getPhone());
   $('#emailDel').val(users[index].getEmail());
-  $('#genderDel').val(users[index].getGender());
+  let gender = users[index].getGender();
+  $('#genderDel').val(gender);
+  $('#imgDel').attr('src',getimgFor(gender));
 }
 
+// edit user info
 $('#editBtn').click(function () {
+  
   $('#nameInput').val($('#nameDel').val());
   $('#numberInput').val($('#phoneDel').val());
   $('#emailInput').val($('#emailDel').val());
+  $('#isAdd').val('0');
 
-  if ($('#genderDel').val().toUpperCase() === "FEMALE") {
-    $("select option").filter(function () {
-      return $(this).text() == 'Female';
-    }).attr('selected', true);
-  } else {
-    $("select option").filter(function () {
-      return $(this).text() == 'Male';
-    }).attr('selected', true);
-  }
+  // $(`#slider-flip-m  option[value=${users[currentIndex].getGender().toUpperCase()}]`).prop("selected", true);
+  $("#slider-flip-m").val(users[currentIndex].getGender().toUpperCase()).change();
+  // if (users[currentIndex].getGender().toUpperCase() === "FEMALE") {
+  //   // $("select option").filter(function () {
+  //   //   return $(this).text() == 'Female';
+  //   // }).attr('selected', true);
+  // } else {
+  //   // $("select option").filter(function () {
+  //   //   return $(this).text() == 'Male';
+  //   // }).attr('selected', true);
+  //   $('#slider-flip-m  option[value="MALE"]').prop("selected", true);
+
+  // }
 });
 
+// delete the user 
 function deleteUser() {
-
-  for (let i = 0; i < users.length; i++) {
-    if (users[i].getName() == $('#nameDel').val() && users[i].getPhone() == $('#phoneDel').val()) {
-      users.splice(i, 1);
-      break;
-    }
-  }
+  users.splice(currentIndex, 1);
   showList();
   $('ul').listview('refresh');
+}
+
+function isMale (x){
+  return x.toUpperCase() === "MALE";
+}
+
+// get img for specific gender
+function getimgFor(x){
+  if (isMale(x))
+      return 'assets/img/male.png';
+  else
+      return 'assets/img/female.png';
+
 }
--- object.js
+++ object.js
@@ -6,27 +6,38 @@ $(document).ready(function () {
     showList();
 
     //on click save button 
+    $('#Add').click(function(){
+        $('#isAdd').val('1');
+        currentIndex =-1;
+        $("#nameInput").val(null);
+        $("#numberInput").val(null);
+        $("#emailInput").val(null);
+    });
     $("#save").click(function () {
 
         //form element
-        var name = document.getElementById("nameInput")
-        var number = document.getElementById("numberInput")
-        var email = document.getElementById("emailInput")
-        var gender = document.getElementById("slider-flip-m")
+        var name = document.getElementById("nameInput");
+        var number = document.getElementById("numberInput");
+        var email = document.getElementById("emailInput");
+        var gender = document.getElementById("slider-flip-m");
+        var isAdd = document.getElementById("isAdd");
+        
 
-        console.log(name.value + " " + number.value);
-        if (findUser(name.value, number.value) == true) {
-            console.log("dddddddddd");
+        if (findUser(name.value, number.value)) {
             alert('This name or number used');
         } else {
+          
+            if(isAdd.value == '0'){
+                deleteUser();
+            }
             var obj = new User(name.value, number.value, email.value, gender.value)
             users.push(obj);
-
             showList();
             $('ul').listview('refresh');
             window.location.href = '#contactList';
         }
     })
+    currentIndex = -1;
 });
 
 $('#cancle').click(function () {
@@ -37,7 +48,7 @@ $('#cancle').click(function () {
 
 function findUser(name, phone) {
     for (let i = 0; i < users.length; i++) {
-        if (users[i].getName() == name || users[i].getPhone() == phone) {
+        if ((users[i].getName() == name || users[i].getPhone() == phone) && i != currentIndex) {
             return true;
         }
     }
